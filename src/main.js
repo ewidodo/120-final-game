7999b0657bb87df64e25f7dee4fe588cca62135b
riko <ewidodo1998@gmail.com>
2020-05-05 17:40:11 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,31 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 1920,
+    height: 1080,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    resolution: window.devicePixelRatio,
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: {
+                x: 0,
+                y: 0,
+            },
+            checkCollision: {
+                left: true,
+                right: true,
+            },
+        },
+    },
+    fps: {
+        target: 60,
+        forceSetTimeOut: true
+    },
+    scene: [],
+};
+
+let game = new Phaser.Game(config);
